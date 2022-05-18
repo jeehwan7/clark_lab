f2be8d08edcfc9fb75beab7c5b573d608006c613
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-05-18 13:27:28 -0400

    Add files via upload

--- visual_stimuli.m
+++ visual_stimuli.m
@@ -1,54 +1,54 @@
 left = 0; % 0: right, 1: left
 div = 0; % 0: converging, 1: diverging
 par = 1; % parity
-c = 72; % column
-r = 100; % row
+x = 72; % width
+y = 100; % number of rows
 
 figure; % plot up all 8 of the 3 point patterns
 
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,1);
 imagesc(mt);
 title('pos, right, conv');
 
 div = 1;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,2);
 imagesc(mt);
 title('pos, right, div');
 
 left = 1; div = 0;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,3);
 imagesc(mt);
 title('pos, left, conv');
 
 left = 1; div = 1;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,4);
 imagesc(mt);
 title('pos, left, div');
 
 par = -1; left = 0; div = 0;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,5);
 imagesc(mt);
 title('neg, right, conv');
 
 div = 1;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,6);
 imagesc(mt);
 title('neg, right, div');
 
 left = 1; div = 0;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,7);
 imagesc(mt);
 title('neg, left, conv');
 
 left = 1; div = 1;
-mt = triple(left, div, par, c, r);
+mt = triple(left, div, par, x, y);
 subplot(2,4,8);
 imagesc(mt);
 title('neg, left, div');
@@ -56,63 +56,63 @@ title('neg, left, div');
 figure; % plot up all 4 of the 2 point patterns
 
 par = 1; left = 0;
-mp = pairwise(left, par, c, r);
+mp = pairwise(left, par, x, y);
 subplot(2,2,1);
 imagesc(mp);
 title('pos, right')
 
 left = 1;
-mp = pairwise(left, par, c, r);
+mp = pairwise(left, par, x, y);
 subplot(2,2,2);
 imagesc(mp);
 title('pos, left')
 
 par = -1; left = 0;
-mp = pairwise(left, par, c, r);
+mp = pairwise(left, par, x, y);
 subplot(2,2,3);
 imagesc(mp);
 title('neg, right')
 
 left = 1;
-mp = pairwise(left, par, c, r);
+mp = pairwise(left, par, x, y);
 subplot(2,2,4);
 imagesc(mp);
 title('neg, left')
 
-function mt = triple(left, div, par, c, r)
+function mt = triple(left, div, par, x, y)
 
     % first row
-    mt(1,:) = (zeros(1,c)-1).^(randi([0 1],[1,c]));
+    mt(1,:) = (zeros(1,x)-1).^(randi([0 1],[1,x]));
     
     % right, converging
-    for t = 2:r
+    for t = 2:y
         mt(t,1) = (-1)^(randi(2)-1);
-        mt(t,2:c) = par*mt(t-1,1:c-1).*mt(t-1,2:c);
+        mt(t,2:x) = par*mt(t-1,1:x-1).*mt(t-1,2:x);
     end
-    
-    % other conditions
+    % right, diverging
     if (left == 0) && (div == 1)
         mt = fliplr(flipud(mt));
+    % left, converging
     elseif (left == 1) && (div == 0)
         mt = fliplr(mt);
+    % left, diverging
     elseif (left == 1) && (div == 1)
         mt = flipud(mt);
     end
 
 end
 
-function mp = pairwise(left, par, c, r)
+function mp = pairwise(left, par, x, y)
 
     % first row
-    mp(1,:) = (zeros(1,c)-1).^(randi([0 1],[1,c]));
+    mp(1,:) = (zeros(1,x)-1).^(randi([0 1],[1,x]));
     
     % right
-    for t = 2:r
+    for t = 2:y
         mp(t,1) = (-1)^(randi(2)-1);
-        mp(t,2:c) = par*mp(t-1,1:c-1);
-    end
-    
-    % other conditions
+        mp(t,2:x) = par*mp(t-1,1:x-1);
+    end 
+    % left
     if left == 1
         mp = fliplr(mp);
     end
--- visual_stimuli_3D.m
+++ visual_stimuli_3D.m
@@ -1,24 +1,124 @@
-% left = 0;
-% div = 0;
-par = 1;
-x = 1000;
-y = 1000;
-z = 120;
+left = 0; % 0: right, 1: left
+div = 0; % 0: converging, 1: diverging
+par = 1; % parity
+x = 1000; % width
+y = 1000; % height
+z = 120; % number of frames
 
-mt = triple(par, x, y, z);
+figure; % plot up all 8 of the 3 point patterns / first n slices across z axis
 
-figure; % slices across z axis
+n = 2;
 
+mt = triple(left, div, par, x, y, z);
 A = permute(mt, [3 2 1]);
-
-for i=1:3
-    subplot(3,1,i);
+for i=1:n
+    subplot(8,n,i);
     imagesc(A(:,:,i));
-    title(strcat('y = ', num2str(i)))
+    title(strcat("pos, right, conv: y = ", num2str(i)))
 end
 
+div = 1;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+n);
+    imagesc(A(:,:,i));
+    title(strcat("pos, right, div: y = ", num2str(i)))
+end
 
-function mt = triple(par, x, y, z)
+left = 1; div = 0;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+2*n);
+    imagesc(A(:,:,i));
+    title(strcat("pos, left, conv: y = ", num2str(i)))
+end
+
+left = 1; div = 1;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+3*n);
+    imagesc(A(:,:,i));
+    title(strcat("pos, left, div: y = ", num2str(i)))
+end
+
+left = 0; div = 0; par = -1;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+4*n);
+    imagesc(A(:,:,i));
+    title(strcat("neg, right, conv: y = ", num2str(i)))
+end
+
+div = 1;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+5*n);
+    imagesc(A(:,:,i));
+    title(strcat("neg, right, div: y = ", num2str(i)))
+end
+
+left = 1; div = 0;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+6*n);
+    imagesc(A(:,:,i));
+    title(strcat("neg, left, conv: y = ", num2str(i)))
+end
+
+left = 1; div = 1;
+mt = triple(left, div, par, x, y, z);
+A = permute(mt, [3 2 1]);
+for i=1:n
+    subplot(8,n,i+7*n);
+    imagesc(A(:,:,i));
+    title(strcat("neg, left, div: y = ", num2str(i)))
+end
+
+figure; % plot up all 4 of the 2 point patterns / first n slices across z axis
+
+left = 0; par = 1;
+mp = pairwise(left, par, x, y, z);
+B = permute(mp, [3 2 1]);
+for i=1:n
+    subplot(4,n,i);
+    imagesc(B(:,:,i));
+    title(strcat("pos, right: y = ", num2str(i)))
+end
+
+left = 1;
+mp = pairwise(left, par, x, y, z);
+B = permute(mp, [3 2 1]);
+for i=1:n
+    subplot(4,n,i+n);
+    imagesc(B(:,:,i));
+    title(strcat("pos, left: y = ", num2str(i)))
+end
+
+left = 0; par = -1;
+mp = pairwise(left, par, x, y, z);
+B = permute(mp, [3 2 1]);
+for i=1:n
+    subplot(4,n,i+2*n);
+    imagesc(B(:,:,i));
+    title(strcat("neg, right: y = ", num2str(i)))
+end
+
+left = 1;
+mp = pairwise(left, par, x, y, z);
+B = permute(mp, [3 2 1]);
+for i=1:n
+    subplot(4,n,i+3*n);
+    imagesc(B(:,:,i));
+    title(strcat("neg, left: y = ", num2str(i)))
+end
+
+function mt = triple(left, div, par, x, y, z)
 
     % first frame
     mt(:,:,1) = (zeros(y,x)-1).^(randi([0 1],[y,x]));
@@ -27,6 +127,33 @@ function mt = triple(par, x, y, z)
     for t = 2:z
         mt(:,1,t) = (-1)^(randi(2)-1);
         mt(:,2:x,t) = par*mt(:,1:x-1,t-1).*mt(:,2:x,t-1);
+    end
+    % right, diverging
+    if (left == 0) && (div == 1)
+        mt = flip(flip(mt, 2), 3);
+    % left, converging
+    elseif (left == 1) && (div == 0)
+        mt = flip(mt, 2);
+    % left, diverging
+    elseif (left == 1) && (div == 1)
+        mt = flip(mt, 3);
+    end
+
+end
+
+function mp = pairwise(left, par, x, y, z)
+
+    % first frame
+    mp(:,:,1) = (zeros(y,x)-1).^(randi([0 1],[y,x]));
+
+    % right
+    for t = 2:z
+        mp(:,1,t) = (-1)^(randi(2)-1);
+        mp(:,2:x, t) = par*mp(:,1:x-1,t-1);
+    end
+    %left
+    if left == 1
+        mp = flip(mp, 2);
     end
 
 end
