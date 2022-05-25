67d07afdea92e979a8c24c5f8339691fdfc485fb
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-05-25 17:42:29 -0400

    Add files via upload

--- Psychophysics/GliderStimuli.m
+++ Psychophysics/GliderStimuli.m
@@ -1,66 +1,108 @@
+AssertOpenGL;
+
 Screen('Preference', 'SkipSyncTests', 2);
+
+% Key Configuration
+KbName('UnifyKeyNames');
+if ismac % Mac
+    lresc = [80,82,41];
+elseif ispc % Windows
+    lresc = [37,39,27];
+else
+    error('PLATFORM NOT SUPPORTED, MAC OR WINDOWS ONLY');
+end
+
+% subjectID = input('Subject ID (0 for debug): ');
+
+%% PARAMETERS
+
+% Resolution Parameters
+param.viewDist = 50; % viewing distance in cm
+param.degperSquare = 0.5; % degrees per square
+
+% Stimulus Parameters
+param.left = 0; % 0 = right, 1 = left
+param.div = 0; % 0 = converging, 1 = diverging
+param.par = 1; % parity
+
+% Temporal Parameters
+param.stimDuration = 1; % duration of stimulus in seconds
+param.framesPerSec = 20; % number of frames per second
+param.preStimWait = 2; % waiting time before stimulus in seconds % !!!NOT YET USED!!!
+
+% Fixation Point Parameters
+param.fpColor = [255,0,0,255]; % !!!NOT YET USED!!!
+param.fpSize = 0.3; % in degrees % !!!NOT YET USED!!!
+
+% Background and Text Luminance
+param.bgLum = 127; % grey % !!!NOT YET USED!!!
+param.textLum = 0; % black % !!!NOT YET USED!!!
+
+% Question Message
+param.question = 'Left(<-) or Right(->)?'; % !!!NOT YET USED!!!
+
+%% RESULTS
+
+%% RUN EXPERIMENT
 
 % Select Screen
 screens = Screen('Screens');
 screenNumber = max(screens);
 
 % Screen Dimensions
-[screenWidthpx,screenHeightpx] = Screen('WindowSize',screenNumber); % in pixels
-[screenWidthmm,screenHeightmm] = Screen('DisplaySize',screenNumber); % in millimeters
+[screenWidthpx,screenHeightpx] = Screen('WindowSize',screenNumber);
+[screenWidthmm,screenHeightmm] = Screen('DisplaySize',screenNumber);
 
-% Pixels, Degrees
-viewDist = 50; % in cm % !!!!!PARAMETER!!!!!
+% Pixel / Visual Angel Correspondence
 pxpercm  = screenWidthpx/screenWidthmm*10; % pixels per cm
-pxperdeg = pxpercm*viewDist*tand(1); % pixels per degree (from the focus, so increasingly inaccurate as we move peripherally)
-degperWidth = screenWidthpx/pxperdeg; % degrees per width
-degperHeight = screenHeightpx/pxperdeg; % degrees per height
-degperSquare = 0.5; % degrees per square % !!!!!PARAMETER!!!!!
-numSquaresX = ceil(degperWidth/degperSquare); % number of squares on X axis, round up to make sure we cover the whole screen
-numSquaresY = ceil(degperHeight/degperSquare); % number of squares on Y axis, round up to make sure we cover the whole screen
-
-%{
-% ONLY TO SEE IF THE DISPLAY WORKS; NOT FOR ACTUAL EXPERIMENT
-mt = permute(mt, [3 2 1]);
-mp = permute(mp, [3 2 1]);
-%}
+pxperdeg = pxpercm*param.viewDist*tand(1); % pixels per degree
+degperWidth = screenWidthpx/pxperdeg; % degrees per width of display
+degperHeight = screenHeightpx/pxperdeg; % degrees per height of display
 
 % Open Window
 [w, rect] = Screen('OpenWindow', screenNumber, 0, [0, 0, screenWidthpx/2, screenHeightpx/2]); % quadrant of screen only
 
-% Refresh Rate
-ifi = Screen('GetFlipInterval', w); % inter frame interval
+% ListenChar(2); % enable listening, suppress output keypresses to command window
 
-% Calculate Wait Frames
-duration = 2; % duration of presentation in seconds % !!!!!PARAMETER!!!!!
-framesPerSec = 15; % frames per second % !!!!!PARAMETER!!!!!
-waitFrames = round(1/ifi/framesPerSec);
+% Stimulus X Axis, Y Axis, and Z Axis
+numSquaresX = ceil(degperWidth/param.degperSquare); % round up to make sure we cover the whole screen
+numSquaresY = ceil(degperHeight/param.degperSquare); % round up to make sure we cover the whole screen
+numFrames = param.stimDuration*param.framesPerSec*2;
+% frame > param.stimDuration*param.framesPerSec (something weird is happening... multiplying by 2 for now... need to fix timing issues)
 
-% Stimulus Settings
-left = 0; % 0: right, 1: left % !!!!!PARAMETER!!!!!
-div = 0; % 0: converging, 1: diverging % !!!!!PARAMETER!!!!!
-par = 1; % parity % !!!!!PARAMETER!!!!!
-x = numSquaresX; % width
-y = numSquaresY; % height
-z = duration*framesPerSec*1.5; % number of frames % frame > duration*framesPerSec WHY? (multiply by 1.5 at the end)
+% For Simplicity
+x = numSquaresX;
+y = numSquaresY;
+z = numFrames;
 
 % Create Stimulus
-mt = triple(left, div, par, x, y, z);
+mt = triple(param, x, y, z);
 mt = 255*(mt+1)/2; % turn all negative ones into zeroes, multiply by 255 for luminance
-mt = repelem(mt,ceil(screenHeightpx/numSquaresY),ceil(screenWidthpx/numSquaresX));
-mp = pairwise(left, par, x, y, z);
+mt = repelem(mt,ceil(screenHeightpx/numSquaresY),ceil(screenWidthpx/numSquaresX)); % repeat elements according to degPerSquare
+
+mp = pairwise(param, x, y, z);
 mp = 255*(mp+1)/2; % turn all negative ones into zeroes, multiply by 255 for luminance
-mp = repelem(mp,ceil(screenHeightpx/numSquaresY),ceil(screenWidthpx/numSquaresX));
+mp = repelem(mp,ceil(screenHeightpx/numSquaresY),ceil(screenWidthpx/numSquaresX)); % repeat elements according to degPerSqaure
+
+% Center of Screen
+[center(1), center(2)] = RectCenter(rect);
+
+% Refresh Rate
+ifi = Screen('GetFlipInterval', w);
+
+% Wait Frames
+waitFrames = round(1/ifi/param.framesPerSec);
 
 % Present Stimulus
-start = GetSecs;
-newvbl = start;
 frame = 1;
+newvbl = GetSecs;
+start = GetSecs;
 
-while GetSecs<start+duration
+while GetSecs < start+param.stimDuration
     oldvbl = newvbl;
     pattern = Screen('MakeTexture', w, mt(:,:,frame));
     Screen('DrawTexture', w, pattern);
-    newvbl = Screen('Flip', w, oldvbl + (waitFrames - 0.5) * ifi);
+    newvbl = Screen('Flip', w, oldvbl + (waitFrames-0.5)*ifi);
     if newvbl ~= oldvbl
         frame = frame+1;
     end
@@ -70,7 +112,7 @@ Screen('Closeall');
 
 % 3D Matrix for Triple Patterns
 % z axis = time
-function mt = triple(left, div, par, x, y, z)
+function mt = triple(param, x, y, z)
 
     % first frame
     mt(:,:,1) = (zeros(y,x)-1).^(randi([0 1],[y,x]));
@@ -78,16 +120,16 @@ function mt = triple(left, div, par, x, y, z)
     % right, converging
     for t = 2:z
         mt(:,1,t) = (-1)^(randi(2)-1);
-        mt(:,2:x,t) = par*mt(:,1:x-1,t-1).*mt(:,2:x,t-1);
+        mt(:,2:x,t) = param.par*mt(:,1:x-1,t-1).*mt(:,2:x,t-1);
     end
     % right, diverging
-    if (left == 0) && (div == 1)
+    if (param.left == 0) && (param.div == 1)
         mt = flip(flip(mt, 2), 3);
     % left, converging
-    elseif (left == 1) && (div == 0)
+    elseif (param.left == 1) && (param.div == 0)
         mt = flip(mt, 2);
     % left, diverging
-    elseif (left == 1) && (div == 1)
+    elseif (param.left == 1) && (param.div == 1)
         mt = flip(mt, 3);
     end
 
@@ -95,7 +137,7 @@ end
 
 % 3D Matrix for Pairwise Patterns
 % z axis = time
-function mp = pairwise(left, par, x, y, z)
+function mp = pairwise(param, x, y, z)
 
     % first frame
     mp(:,:,1) = (zeros(y,x)-1).^(randi([0 1],[y,x]));
@@ -103,10 +145,10 @@ function mp = pairwise(left, par, x, y, z)
     % right
     for t = 2:z
         mp(:,1,t) = (-1)^(randi(2)-1);
-        mp(:,2:x, t) = par*mp(:,1:x-1,t-1);
+        mp(:,2:x, t) = param.par*mp(:,1:x-1,t-1);
     end
     %left
-    if left == 1
+    if param.left == 1
         mp = flip(mp, 2);
     end
 
