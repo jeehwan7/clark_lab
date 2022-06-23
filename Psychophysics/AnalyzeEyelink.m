8f991eeec9b186169d70e0c1cfe98b6f7c9db85d
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-06-23 17:01:37 -0400

    Add files via upload

--- Psychophysics/AnalyzeEyelink.m
+++ Psychophysics/AnalyzeEyelink.m
@@ -1,17 +1,69 @@
-% trialNumber = 1;
+% COPY AND PASTE FILE NAME BELOW ('... .mat')
+load('.mat','results');
 
-% fileName = ['Trial',num2str(trialNumber),'.edf'];
+numTrials = height(struct2table(results));
 
-% trial = Edf2Mat(fileName);
+trialNumbers = zeros(11,numTrials/11);
 
-stimulusStartTime = trial.Events.Messages.time(find(trial.Events.Messages.info == 'STIMULUS_START'));
+coherences = vertcat(results.coherence);
 
-timeposX = zeros(size(trial.Samples.time,1),2);
-timeposX(1:size(timeposX,1),1) = trial.Samples.time;
-timeposX(1:size(timeposX,1),2) = trial.Samples.posX;
+for i = 1:11
+    trialNumbers(i,:) = find(coherences == round((i-1)*0.1,1));
+end
 
+for j = 1:11
+    
+    figure;
+    tiledlayout(2,1)
+    nexttile
 
+    % time vs. eye position
 
-% stimulusEndTime = trial.Events.Messages.time(find(trial.Events.Messages.info == 'STIMULUS_END'));
+    for k = 1:size(trialNumbers,2)
 
-% find(trial.Samples.time == stimulusStartTime)
+        data = Edf2Mat(['Trial',num2str(trialNumbers(j,k)),'.edf']);
+        
+        index = find(strcmp(data.Events.Messages.info,'STIMULUS_START'));
+        
+        stimulusStartTime = data.Events.Messages.time(index);
+        
+        normalizedTime = data.Samples.time - stimulusStartTime;
+
+        plot(normalizedTime,data.Samples.posX);
+    
+        hold on
+
+    end
+    
+    xline(0,'--');
+    title(['Coherence = ',num2str(round((j-1)*0.1,1))]);
+    xlabel('t');
+    ylabel('x')
+    hold off
+    
+    nexttile
+
+    % time vs. gradient
+
+    for k = 1:size(trialNumbers,2)
+
+        data = Edf2Mat(['Trial',num2str(trialNumbers(j,k)),'.edf']);
+        
+        index = find(strcmp(data.Events.Messages.info,'STIMULUS_START'));
+        
+        stimulusStartTime = data.Events.Messages.time(index);
+        
+        normalizedTime = data.Samples.time - stimulusStartTime;
+    
+        plot(normalizedTime(2:end),diff(data.Samples.posX));
+    
+        hold on
+
+    end
+    
+    xline(0,'--');
+    xlabel('t');
+    ylabel('dx/dt')
+    hold off
+
+end
