fc748e79255b30ade5ecd72669407e726f88dd9c
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-07-07 15:30:07 -0400

    Add files via upload

---
+++ Psychophysics/Functions/createDeltaThetabyCoherencebyTimeInterval.m
@@ -0,0 +1,38 @@
+function deltaThetabyCoherencebyTimeInterval = createDeltaThetabyCoherencebyTimeInterval(interval, range, deltaThetabyCoherence, dThetabyTrialWithoutSaccades, coherences, directions)
+
+    deltaThetabyCoherencebyTimeInterval = zeros(range/interval,2);
+    deltaThetabyCoherencebyTimeInterval(:,2) = interval:interval:range;
+    r = 1;
+    for tt = 1:interval:range-interval+1
+        for ii = 1:size(deltaThetabyCoherence,1)
+            % sum dtheta to get deltatheta
+            deltaThetabyCoherence(ii,1) = sum(dThetabyTrialWithoutSaccades(ii,1:tt+interval-1),'omitnan');
+            % render the sum NaN if it was from an unreadable edf file
+            % (i.e. if all of the values were NaN because the sum comes out as zero)
+            if sum(~isnan(dThetabyTrialWithoutSaccades(ii,:))) == 0
+                deltaThetabyCoherence(ii,1) = NaN;
+            end
+            % if it was a left trial, multiply the coherence by -1
+            if directions(ii,1) == 1
+                deltaThetabyCoherence(ii,2) = coherences(ii,1);
+            elseif directions(ii,1) == -1
+                deltaThetabyCoherence(ii,2) = -coherences(ii,1);
+            end
+        end
+        x = deltaThetabyCoherence(:,2);
+        y = deltaThetabyCoherence(:,1);
+        index = isnan(y);
+        p = polyfit(x(~index),y(~index),1);
+        deltaThetabyCoherencebyTimeInterval(r,1) = p(1);
+        r = r+1;
+    end
+    deltaThetabyCoherencebyTimeInterval(:,1) = deltaThetabyCoherencebyTimeInterval(:,1)/1000;
+    % scatter plot
+    figure;
+    scatter(deltaThetabyCoherencebyTimeInterval(:,2),deltaThetabyCoherencebyTimeInterval(:,1));
+    title('\Delta\theta / Coherence');
+    yline(0,'--');
+    xlabel(['Time Interval = ',num2str(interval),' (ms)']);
+    ylabel('\Delta\theta (rad) / Coherence');
+
+end
---
+++ Psychophysics/Functions/runFunctions.m
@@ -0,0 +1,38 @@
+fileName = 'Subject0_2022.06.28_1406.mat';
+% create matrices for data
+[numTrials, posXbyTrial, paramsbyTrial, coherences, directions, responses, responseTimes] = createMatrices(fileName);
+
+% PLOT posX by Trial
+plotSuccess = plotPosX(numTrials, coherences, posXbyTrial); 
+
+% create dTheta by Trial
+dThetabyTrial = createDThetabyTrial(posXbyTrial);
+
+% PLOT dTheta by Trial
+plotSuccess = plotDTheta(numTrials, coherences, dThetabyTrial);
+
+% set threshold and cut time to remove saccades
+param.threshold = 57; % (deg/s)
+param.cutTime = 15; % (ms)
+% create dTheta by Trial without saccades
+dThetabyTrialWithoutSaccades = removeSaccades(dThetabyTrial, param.threshold, param.cutTime);
+
+% PLOT dTheta by Trial without Saccades
+plotSuccess = plotDTheta(numTrials, coherences, dThetabyTrialWithoutSaccades);
+
+% create and PLOT DeltaTheta by Coherence
+deltaThetabyCoherence = createDeltaThetabyCoherence(numTrials, dThetabyTrialWithoutSaccades, coherences, directions, responses);
+
+%{
+% set interval and range for DeltaTheta by Coherence by Time Interval
+param.interval = 1; % factor of range
+param.range = 200; % max 1000
+% create and PLOT DeltaTheta by Coherence by Time Interval
+deltaThetabyCoherencebyTimeInterval = createDeltaThetabyCoherencebyTimeInterval(param.interval, param.range, deltaThetabyCoherence, dThetabyTrialWithoutSaccades, coherences, directions);
+%}
+
+% create and PLOT P(Right) by Coherence
+prightbyCoherence = createPrightbyCoherence(responses, coherences, directions);
+
+% crate and PLOT Skewness by coherence
+skewnessbyCoherence = createSkewnessbyCoherence(numTrials, dThetabyTrial, coherences, directions);
