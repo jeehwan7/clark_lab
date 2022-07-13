8e562d57bdb2b40081f3a513db811f8b5b761484
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-07-13 16:43:49 -0400

    Add files via upload

---
+++ Psychophysics/Coherence Functions/createDeltaThetabyCoherence.m
@@ -0,0 +1,36 @@
+function deltaThetabyCoherence = createDeltaThetabyCoherence(numTrials, dThetabyTrialWithoutSaccades, coherences, directions, responses)
+
+    deltaThetabyCoherence = zeros(numTrials,2); % deltatheta, coherence (in that order)
+
+    for ii = 1:size(deltaThetabyCoherence,1)
+        % sum dtheta to get deltatheta
+        deltaThetabyCoherence(ii,1) = sum(dThetabyTrialWithoutSaccades(ii,:),'omitnan');
+        % render the sum NaN if it was from an unreadable edf file
+        % (i.e. if all of the values were NaN because the sum comes out as zero)
+        if sum(~isnan(dThetabyTrialWithoutSaccades(ii,:))) == 0
+            deltaThetabyCoherence(ii,1) = NaN;
+        end
+        % if it was a left trial, multiply the coherence by -1
+        if directions(ii,1) == 1
+            deltaThetabyCoherence(ii,2) = coherences(ii,1);
+        elseif directions(ii,1) == -1
+            deltaThetabyCoherence(ii,2) = -coherences(ii,1);
+        end
+    end
+    deltaThetabyCoherence(:,1) = deltaThetabyCoherence(:,1)/1000;
+    % index
+    leftResponseIndex = find(responses == -1);
+    rightResponseIndex = find(responses == 1);
+    % scatter plot
+    figure;
+    scatter(deltaThetabyCoherence(leftResponseIndex,2),deltaThetabyCoherence(leftResponseIndex,1));
+    hold on
+    scatter(deltaThetabyCoherence(rightResponseIndex,2),deltaThetabyCoherence(rightResponseIndex,1));
+    hold off
+    title('\Delta\theta (Excluding Saccades)');
+    yline(0,'--');
+    xlabel('Coherence');
+    ylabel('\Delta\theta (deg)');
+    legend({'response = left','response = right'},'Location','southeast');
+    
+end
