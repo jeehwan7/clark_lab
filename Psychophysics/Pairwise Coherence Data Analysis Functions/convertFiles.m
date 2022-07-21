6851c276f526da376fd684409f71172102646306
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-07-21 13:54:51 -0400

    Add files via upload

---
+++ Psychophysics/Pairwise Coherence Data Analysis Functions/convertFiles.m
@@ -0,0 +1,21 @@
+function edf2matErrors = convertFiles(fileName)
+    
+    %% LOAD RESULTS
+    load(fileName,'results');
+    numTrials = height(struct2table(results));
+
+    %% CONVERT EDF FILES TO MAT FILES
+    mkdir MatFiles;
+    edf2matErrors = [];
+    
+    for f = 1:numTrials
+        try
+            data = Edf2Mat(['eyelink/','Trial',num2str(f),'.edf']);
+            save(['MatFiles/Trial',num2str(f),'.mat'],'data');
+        catch % if edf file is unreadable
+            warning(['Trial ',num2str(f),' will be excluded from the results.']);
+            edf2matErrors = [edf2matErrors, f];
+        end
+    end
+
+end
