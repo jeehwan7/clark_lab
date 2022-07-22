803317cdedb18c08351d8f1994b2a61b2aedb1f0
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-07-22 17:57:33 -0400

    Add files via upload

---
+++ Psychophysics/PairwiseCoherenceDataAnalysisFunctions/createDThetabyTrial.m
@@ -0,0 +1,11 @@
+function dThetabyTrial = createDThetabyTrial(posXbyTrial)
+
+    % according to the EyeLink settings and my own measurements
+    viewDistmm = 560;
+    screenWidthpx = 1920;
+    screenWidthmm = 600;
+    % trigonometry
+    thetabyTrial = atan((posXbyTrial-960)/screenWidthpx*screenWidthmm/viewDistmm);
+    dThetabyTrial = diff(thetabyTrial,1,2)/pi*180*1000; % diff(X,n,dim); % deg/s
+
+end
