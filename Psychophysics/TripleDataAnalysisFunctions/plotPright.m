4182070f5b8641fdb7855f651d84c670239699db
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2022-10-25 03:16:15 -0400

    Add files via upload

--- Psychophysics/TripleDataAnalysisFunctions/plotPright.m
+++ Psychophysics/TripleDataAnalysisFunctions/plotPright.m
@@ -4,8 +4,23 @@ function Q = plotPright(Q)
 
     pc = Q.PrightPairwiseCoherence;
 
+    %{
     figure;
     scatter(pc,"Coherence","P(Right)");
+    yline(0.5,'--');
+    xlabel('coherence');
+    ylabel('P(Right)');
+    title('Pairwise Coherence');
+    %}
+
+    % GLM Fit
+    figure;
+    x = Q.PrightPairwiseCoherence.Coherence;
+    n = Q.PrightPairwiseCoherence.("Trial Frequency");
+    y = Q.PrightPairwiseCoherence.("Right Frequency");
+    b = glmfit(x,[y n],'binomial','Link','probit');
+    yfit = glmval(b,x,'probit','Size',n);
+    plot(x,y./n,'o',x,yfit./n,'-');
     yline(0.5,'--');
     xlabel('coherence');
     ylabel('P(Right)');
