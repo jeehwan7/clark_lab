86348f413ebd719a7d962f8a1b4318921ad67ad4
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2024-06-26 12:04:46 -0400

    Add files via upload

---
+++ Psychophysics/EyeLink_RunTripleCoherenceImpulseExperiment.m
@@ -0,0 +1,393 @@
+AssertOpenGL;
+
+Screen('Preference', 'SkipSyncTests', 0);
+
+%% KEY CONFIGURATION
+
+KbName('UnifyKeyNames');
+if ismac
+    lresc = [80,82,41];
+elseif isunix
+    lresc = [KbName('LeftArrow'),KbName('RightArrow'),KbName('ESCAPE')];
+elseif ispc
+    lresc = [37,39,27];
+else
+    error('PLATFORM NOT SUPPORTED; MAC OS X, WINDOWS, OR LINUX ONLY');
+end
+
+%% PARAMETERS
+
+% Resolution Parameters
+param.viewDist = 52; % viewing distance in cm
+param.degPerSquare = 1; % degrees per check
+
+% Temporal Parameters
+param.stimDuration = 10; % duration of stimulus in seconds
+param.framesPerSec = 60; % number of frames we want per second
+                         % Set this to a factor of the screen frame rate.
+                         % Otherwise glitching will occur
+param.preStimWait = 2; % duration of fixation point in seconds
+
+% Number of Blocks
+param.numBlocks = 10;
+param.numTrialsPerBlock = 1;
+
+% Fixation Point Parameters
+param.fpColor = [255,0,0,255]; % red
+param.fpSize = 0.3; % in degrees
+
+% Background and Text Parameters
+param.bgLum = 255/2; % grey
+param.textSize = 30;
+param.textLum = 0; % black
+
+param.parity = 1;
+param.diverging = 1;
+param.fracCoherence = 1;
+
+%% RUN EXPERIMENT
+
+% REGISTER SUBJECT
+subjectID = input('SUBJECT ID: ');
+
+% Select Screen
+screens = Screen('Screens');
+screenNumber = max(screens);
+
+% Screen Dimensions
+[screenWidthpx,screenHeightpx] = Screen('WindowSize',screenNumber);
+[screenWidthmm,screenHeightmm] = Screen('DisplaySize',screenNumber);
+
+% Degree / Pixel Correspondence
+pxPermm = mean([screenWidthpx/screenWidthmm, screenHeightpx/screenHeightmm]); % taking the average (almost identical)
+
+fpmm = tand(param.fpSize)*param.viewDist*10;
+fppx = round(pxPermm*fpmm); % number of pixels for fixation point
+
+mmPerSquare = tand(param.degPerSquare)*param.viewDist*10;
+pxPerSquare = round(pxPermm*mmPerSquare); % number of pixels per check
+
+numSquaresX = ceil(screenWidthpx/pxPerSquare); % round up to ensure we cover the whole screen
+numSquaresY = ceil(screenHeightpx/pxPerSquare); % round up to ensure we cover the whole screen
+numFrames = param.stimDuration*param.framesPerSec;
+
+% Save Results File
+if ~isfolder('./triplecoherenceimpulseresults'); mkdir('./triplecoherenceimpulseresults'); end
+startTime = datestr(now,'yyyy.mm.dd_HHMM');
+mkdir(['./triplecoherenceimpulseresults/','Subject',num2str(subjectID),'_',startTime]);
+save(['./triplecoherenceimpulseresults/','Subject',num2str(subjectID),'_',startTime,'/','Subject',num2str(subjectID),'_',startTime,'.mat'],'subjectID','startTime','param','pxPermm','screenWidthpx');
+
+% Create EyeLink Data Folder (edf files)
+if ~isfolder(['./triplecoherenceimpulseresults/','Subject',num2str(subjectID),'_',startTime,'/EdfFiles'])
+    mkdir(['./triplecoherenceimpulseresults/','Subject',num2str(subjectID),'_',startTime,'/EdfFiles']);
+end
+
+% OPEN WINDOW
+[w, rect] = Screen('OpenWindow', screenNumber, param.bgLum, [0, 0, screenWidthpx, screenHeightpx]);
+
+% Center of Screen
+[center(1), center(2)] = RectCenter(rect);
+
+% Blending Mode
+Screen('BlendFunction', w, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+% Refresh Rate
+ifi = Screen('GetFlipInterval', w);
+
+% Wait Frames
+waitFrames = round(1/ifi/param.framesPerSec);
+
+% Provide Eyelink with details about the graphics environment and perform some initializations.
+% The information is returned in a structure that also contains useful defaults and control codes
+% (e.g. tracker state bit and Eyelink key values).
+el = EyelinkInitDefaults(w);
+el.targetbeep = 0;
+el.feedbackbeep = 0;
+EyelinkUpdateDefaults(el);
+
+ListenChar(2); % enable listening, suppress output to MATLAB command window
+
+dummymode = 0; % Set to 1 to initialize in dummymode.
+
+% Initialize connection with the Eyelink Gazetracker.
+% Exit program if this fails.
+if ~EyelinkInit(dummymode,1)
+    fprintf('Eyelink Init aborted.\n');
+    cleanup;
+    return;
+end
+
+[v, vs] = Eyelink('GetTrackerVersion');
+fprintf('Running experiment on a ''%s'' tracker.\n',vs);
+
+% EYELINK CALIBRATION
+EyelinkDoTrackerSetup(el,13);
+
+% WELCOME
+msg = [
+    'Welcome!\n\n',...
+    'Press any key to continue'
+    ];
+drawText(w,msg,param.textSize,param.textLum);
+Screen('Flip',w);
+WaitSecs(0.5);
+KbWait;
+
+% INSRUCTIONS
+msg = [
+    'INSTRUCTIONS:\n\n',...
+    'A red dot will appear in the center of the screen.\n',...
+    'Fixate on that dot until the stimulus appears.\n\n',...
+    'Press any key to begin'
+    ];
+drawText(w,msg,param.textSize,param.textLum);
+Screen('Flip',w);
+WaitSecs(0.5);
+KbWait;
+
+abortFlag = 0;
+
+stimuli = cell(1,numFrames); % to save all the frames for each trial (1 or -1 for each check)
+directions = NaN(1,numFrames); % column: trial number, row: frame number
+results = struct;
+
+for ii = 1:param.numBlocks
+    
+    if ii ~= 1
+        % DO YOU WISH TO CONTINUE
+        msg = ['Do you wish to continue?\n\n',...
+            'YES: right arrow key\n\n' ...
+            'NO: escape key'];
+        drawText(w,msg,param.textSize,param.textLum);
+        Screen('Flip',w);
+        WaitSecs(0.5);
+        while 1
+            [~,keyCode] = KbWait;
+            if keyCode(lresc(2)) == 1
+                break
+            elseif keyCode(lresc(3)) == 1
+                abortFlag = 1;
+                break
+            end
+        end
+    end
+
+    if abortFlag == 1; break; end
+
+    % PREPARING TEXTURES
+    msg = ['Preparing textures...\n\n',...
+        'Please be patient'
+        ];
+    drawText(w,msg,param.textSize,param.textLum);
+    Screen('Flip',w);
+
+    % Create All Textures for This Block
+    squares = cell(param.numTrialsPerBlock,numFrames); % for storing matrices
+    textures = cell(param.numTrialsPerBlock,numFrames); % for storing texture indices
+
+    for jj = 1:param.numTrialsPerBlock
+        [tripleSquares, dir] = triple(numSquaresX, numSquaresY, numFrames, param.parity, param.diverging, param.fracCoherence);
+        tripleMatrix = 255*(tripleSquares+1)/2; % turn all negative ones into zeroes, multiply by 255 for luminance (black or white)
+        tripleMatrix = repelem(tripleMatrix,pxPerSquare,pxPerSquare); % "zoom in" according to degPerSquare
+
+        directions((ii-1)*param.numTrialsPerBlock+jj,:) = dir;
+
+        for kk = 1:numFrames
+            squares{jj,kk} = tripleSquares(:,:,kk);
+            textures{jj,kk} = Screen('MakeTexture',w,tripleMatrix(:,:,kk));
+        end
+    end
+
+    % PREPARATION COMPLETE
+    if ii == 1
+        msg = ['Preparation complete\n\n',...
+            'Press any key to start'
+            ];
+        drawText(w,msg,param.textSize,param.textLum);
+        Screen('Flip',w);
+        WaitSecs(0.5);
+        KbWait;
+    else
+        msg = ['Preparation complete\n\n,...' ...
+            'Moving onto drift correction...'
+            ];
+        drawText(w,msg,param.textSize,param.textLum);
+        Screen('Flip',w);
+        WaitSecs(2);
+        
+        % EYELINK DRIFT CORRECTION
+        EyelinkDoDriftCorrection(el);
+
+        msg = ['Drift correction complete\n\n',...
+            'Press any key to start'
+            ];
+        drawText(w,msg,param.textSize,param.textLum);
+        Screen('Flip',w);
+        WaitSecs(0.5);
+        KbWait;
+    end
+
+    for ss = 1:param.numTrialsPerBlock
+        
+        % Open edf file
+        edfFile = ['Trial',num2str((ii-1)*param.numTrialsPerBlock+ss),'.edf'];
+        Eyelink('Openfile',edfFile);
+
+        % columns for indivFrameInfo table
+        frame = permute(1:numFrames,[2 1]);
+        onsetTime = NaN(numFrames,1); % onset time of frame
+        duration = NaN(numFrames,1); % duration of frame
+        timeElapsed = NaN(numFrames,1); % time elapsed since stimulus onset (frame 1 onset)
+
+        % Start recording eye position
+        Eyelink('StartRecording');
+
+        % PRESENT FIXATION POINT
+        Screen('DrawDots', w, [0,0], fppx, param.fpColor, center, 1);
+        vbl = Screen('Flip', w);
+
+        % PRESENT STIMULUS
+        Screen('DrawTexture', w, textures{ss,1}); % frame 1
+        vbl = Screen('Flip', w, vbl + param.preStimWait-0.5*ifi); % duration of dot presentation utilized here
+
+        Eyelink('Message','STIMULUS_START'); % mark stimulus start
+
+        onsetTime(1) = vbl;
+
+        for qq = 2:numFrames % frames 2 to last
+            vblPrevious = vbl;
+            Screen('Drawtexture', w, textures{ss,qq});
+            vbl = Screen('Flip', w, vbl + (waitFrames-0.5)*ifi);
+
+            onsetTime(qq) = vbl;
+            timeElapsed(qq) = vbl-onsetTime(1);
+            duration(qq-1) = vbl-vblPrevious;
+        end
+
+        stimulusEndTime = Screen('Flip', w, vbl + (waitFrames-0.5)*ifi);
+
+        Eyelink('Message','STIMULUS_END'); % mark stimulus end
+
+        duration(numFrames) = stimulusEndTime-vbl;
+
+        for aa = 1:numFrames
+            Screen('Close',textures{ss,aa});
+        end
+
+        % Stop recording eye position
+        Eyelink('StopRecording');
+        Eyelink('CloseFile');
+
+        % Download edf file
+        try
+            fprintf('Receiving data file ''%s''\n',edfFile);
+            status = Eyelink('ReceiveFile');
+            if status > 0
+                fprintf('ReceiveFile status %d\n',status);
+            end
+            if 2 == exist(edfFile,'file')
+                fprintf('Data file ''%s'' can be found in ''%s''\n',edfFile,pwd);
+            end
+            movefile('test.edf',fullfile(curr_sub_dir,['run' num2str(run_num) '.edf']));
+        catch rdf
+            fprintf('Problem receiving data file ''%s''\n',edfFile);
+            rdf;
+        end
+
+        %% UPDATE 'stimuli' cell
+        stimuli((ii-1)*param.numTrialsPerBlock+ss,:) = squares(ss,:);
+        
+        %% UPDATE 'results' structure
+
+        % Trial Number
+        results((ii-1)*param.numTrialsPerBlock+ss).trialNumber = (ii-1)*param.numTrialsPerBlock+ss;
+
+%         % Stimulus Start Time
+%         results((ii-1)*param.numTrialsPerBlock+ss).stimulusStartTime = onsetTime(1);
+%         % Stimulus End Time
+%         results((ii-1)*param.numTrialsPerBlock+ss).stimulusEndTime = stimulusEndTime;
+
+        % Stimulus Duration
+        results((ii-1)*param.numTrialsPerBlock+ss).stimulusDuration = stimulusEndTime-stimulusStartTime;
+
+        % Individual Frame Information
+        results((ii-1)*param.numTrialsPerBlock+ss).indivFrameInfo = table(frame,onsetTime,duration,timeElapsed);
+
+    end
+    
+end
+
+if abortFlag == 1; disp('ABORTING EXPERIMENT...'); end
+
+% SAVING RESULTS
+msg = 'Saving results...';
+drawText(w,msg,param.textSize,param.textLum);
+Screen('Flip',w);
+WaitSecs(1);
+
+% Append 'stimuli', 'directions', and 'results'
+save(['./triplecoherenceimpulseresults/','Subject',num2str(subjectID),'_',startTime,'/','Subject',num2str(subjectID),'_',startTime,'.mat'],'stimuli','directions','results','-append');
+
+% END
+msg = [
+    'Thank you for participating!\n\n',...
+    'Press any key to close'];
+drawText(w,msg,param.textSize,param.textLum);
+Screen('Flip',w);
+WaitSecs(0.5);
+KbWait;
+cleanup;
+
+% 3D Matrix for Triple Pattern with Randomly Switching Directions
+function [mt, dir] = triple(x, y, z, par, div, fracCoherence)
+
+    dir = 2*randi([0 1],[1 z-1])-1;
+
+    % if diverging, flip the direction matrix (without the initial NaN) first
+    if div == 0
+        temp_dir = dir;
+    elseif div == 1
+        temp_dir = flip(dir, 2);
+    end
+
+    dir = [NaN, dir];
+
+    temp_dir = [NaN, temp_dir];
+
+    % first frame
+    mt(:,:,1) = (zeros(y,x)-1).^(randi([0 1],[y,x]));
+    
+    for t = 2:z
+        if temp_dir(1,t) == 1 % right
+            mt(:,1,t) = (zeros(y,1)-1).^(randi([0 1],[y,1])); % new leftmost column
+            mt(:,2:x,t) = par*mt(:,1:x-1,t-1).*mt(:,2:x,t-1); % apply correlation
+        elseif temp_dir(1,t) == -1 % left
+            mt(:,end,t) = (zeros(y,1)-1).^(randi([0 1],[y 1])); % new rightmost column
+            mt(:,1:x-1,t) = par*mt(:,1:x-1,t-1).*mt(:,2:x,t-1); % apply correlation
+        end
+    end
+
+    if div == 1 % have to flip twice for diverging
+        mt = flip(flip(mt,2),3);
+    end
+
+    % apply coherence
+    for t = 2:z
+        indexRandom = randperm(x*y,x*y-round(x*y*fracCoherence));
+        mt(x*y*(t-1)+indexRandom) = 2*(rand(1,size(indexRandom,2))>0.5)-1;
+    end
+
+end
+
+% Draw Text
+function drawText(window,text,textSize,textLum)
+    Screen('Textsize',window,textSize);
+    DrawFormattedText(window,text,'center','center',textLum);
+end
+
+function cleanup
+    % Shutdown Eyelink
+    Eyelink('Shutdown');
+    sca;
+    ListenChar(0);
+end
