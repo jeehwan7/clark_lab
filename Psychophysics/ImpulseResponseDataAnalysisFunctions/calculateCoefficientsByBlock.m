ad74e3a0714dfb616275f4534a239c7ff21cb9c5
JeeHwan Kim <105673278+jeehwan7@users.noreply.github.com>
2024-07-23 22:32:14 -0400

    Add files via upload

--- Psychophysics/ImpulseResponseDataAnalysisFunctions/calculateCoefficientsByBlock.m
+++ Psychophysics/ImpulseResponseDataAnalysisFunctions/calculateCoefficientsByBlock.m
@@ -7,11 +7,11 @@ function Q = calculateCoefficientsByBlock(Q,param,directions)
         tempD = NaN(param.numTrialsPerBlock*Q.updateRate*Q.stimDuration,Q.numCoefficients); % all the directions
         tempV = NaN(param.numTrialsPerBlock*Q.updateRate*Q.stimDuration,1); % all the velocities      
 
-        for ll = (1:10)+(kk-1)*10
+        for ll = (1:param.numTrialsPerBlock)+(kk-1)*param.numTrialsPerBlock
             for mm = 1:(Q.updateRate*Q.stimDuration)
                 if ~isnan(Q.downSampled(ll,mm))
-                    tempV(mod(ll-1,10)*Q.updateRate*Q.stimDuration+mm) = Q.downSampled(ll,mm);
-                    tempD(mod(ll-1,10)*Q.updateRate*Q.stimDuration+mm,:) = flip(directions(ll,(mm-Q.numCoefficients):(mm-1)));
+                    tempV(mod(ll-1,param.numTrialsPerBlock)*Q.updateRate*Q.stimDuration+mm) = Q.downSampled(ll,mm);
+                    tempD(mod(ll-1,param.numTrialsPerBlock)*Q.updateRate*Q.stimDuration+mm,:) = flip(directions(ll,(mm-Q.numCoefficients):(mm-1)));
                 end
             end
         end
@@ -34,24 +34,23 @@ function Q = calculateCoefficientsByBlock(Q,param,directions)
     b = [1/4 1/2 1/4];
     a = 1;
 
-    x = (1:Q.numCoefficients)*1000/Q.updateRate;
-    color = [0 0.4470 0.7410;
-             0.8500 0.3250 0.0980;
-             0.9290 0.6940 0.1250;
-             0.4940 0.1840 0.5560;
-             0.4660 0.6740 0.1880];
-
     % plot
     figure;
+    x = (1:Q.numCoefficients)*1000/Q.updateRate;
+    color = colormap(cool(param.numBlocks)); % colormap
+    leg = cell(param.numBlocks*2,1); % legend
     for ii = 1:param.numBlocks
-        s = std(Q.tbtCoefficients((1:10)+(ii-1)*10,:),0,1);
+        s = std(Q.tbtCoefficients((1:param.numTrialsPerBlock)+(ii-1)*param.numTrialsPerBlock,:),0,1);
         sem = s/sqrt(param.numTrialsPerBlock);
 
         z = filtfilt(b,a,Q.bbbCoefficients(ii,:));
 
-        patch([x fliplr(x)],[z-sem  fliplr(z+sem)],color(ii,:),'FaceAlpha',0.2,'EdgeColor','none');
+        patch([x fliplr(x)],[z-sem  fliplr(z+sem)],color(ii),'FaceAlpha',0.2,'EdgeColor','none');
+        leg{ii*2-1} = '';
         hold on
+
         plot(x,z,'Color',color(ii,:),'LineWidth',2);
+        leg{ii*2} = ['block ',num2str(ii)];
         hold on
     end
     hold off
@@ -60,7 +59,7 @@ function Q = calculateCoefficientsByBlock(Q,param,directions)
     title('Impulse Response');
     xlabel('-t (ms)');
     ylabel('weighting');
-    legend('','block 1','','block 2','','block 3','','block 4','','block 5');
+    legend(leg)
     legend('Location','northeast');
 
 end
